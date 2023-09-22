9ba593a03a4680646033c50a71aeb47c6eb00ae7
Egor Shievanov <shievanov@bk.ru>
2023-09-22 22:28:13 +0300

    Реализовал 2 шаблона сайта home и contact для получения данных от пользователя

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        # а также передается информация, которую заполнил пользователь
+        print(name, email, message)
+    return render(request, 'catalog/contact.html')
